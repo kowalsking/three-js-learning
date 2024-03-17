000bb117a441fdd29e7191bc1d4e7ffcca42e00a
Hence <kovvalsking@gmail.com>
2024-03-17 14:23:50 +0200

    Fundamentals

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
-import * as THREE from "https://unpkg.com/three/build/three.module.js";
+import * as THREE from 'https://unpkg.com/three/build/three.module.js'
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+function main() {
+  const canvas = document.querySelector('#c')
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+  
+  // camera
+  const fov = 75
+  const aspect = 2
+  const near = 0.1
+  const far = 5
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+  camera.position.z = 2
 
-const scene = new THREE.Scene();
+  const scene = new THREE.Scene()
 
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-const points = [];
-points.push( new THREE.Vector3( -10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
+  const boxWidth = 1
+  const boxHeight = 1
+  const boxDepth = 1
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
 
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
 
-const line = new THREE.Line( geometry, material );
-scene.add( line );
-renderer.render( scene, camera );
+  const color = 0xffffff;
+  const intensity = 3;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
+
+  const cubes = [
+    makeInstance(geometry, 0x44aa88,  0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844,  2),
+  ]
+
+  renderer.render(scene, camera)
+
+  function render(time) {
+    time *= 0.001
+  
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1
+      const rot = time * speed
+      cube.rotation.x = rot
+      cube.rotation.y = rot
+    })
+  
+    renderer.render(scene, camera)
+  
+    requestAnimationFrame(render)
+  }
+  
+  requestAnimationFrame(render)
+
+
+  function makeInstance(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({ color })
+
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+
+    cube.position.x = x
+
+    return cube
+  }
+}
+
+main()
+
