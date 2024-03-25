aabbe91da35852a0927c69f15b59af909ae2dd83
Hence <kovvalsking@gmail.com>
2024-03-25 22:15:27 +0200

    cube grouping tut

--- src/script.js
+++ src/script.js
@@ -9,19 +9,30 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
-// Scale
-mesh.scale.set(2, 0.5, 0.5)
+const group = new THREE.Group()
+scene.add(group)
 
-// Axes helper
-const axesHelper = new THREE.AxesHelper(3)
-scene.add(axesHelper)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff00ff })
+)
+cube2.position.x = -2
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xffff00 })
+)
+cube3.position.x = 2
+group.add(cube3)
 
-mesh.position.set(0.7, -0.6, 1)
+group.position.y = 2
+group.rotation.z = 1
 
 /**
  * Sizes
