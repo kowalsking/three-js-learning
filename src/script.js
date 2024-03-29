e5693848ddff4ac1771a2d234aceda99afe784de
Hence <kovvalsking@gmail.com>
2024-03-29 21:26:58 +0200

    try smth

--- src/script.js
+++ src/script.js
@@ -1,6 +1,19 @@
 import * as THREE from 'three'
 
 /**
+ * Cursor
+ */
+
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
+/**
  * Base
  */
 // Canvas
@@ -24,9 +37,9 @@ scene.add(mesh)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -44,7 +57,12 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    camera.position.x = cursor.x * 10
+    camera.position.y = cursor.y * 10
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
